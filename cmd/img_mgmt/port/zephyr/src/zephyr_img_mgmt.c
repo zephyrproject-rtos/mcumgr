7d96899240f295797011f5fb9f749bf1cf8e19e3
Dominik Ermel <dominik.ermel@nordicsemi.no>
2020-12-10 15:17:04 +0000

    zephyr: Add support for image ROM address verification
    The commit adds IMAGE_F_ROM_FIXED_ADDR flag that allows to use
    ih_load_addr to identify the flash address the image is intendant to
    start at.
    The code, that supports this flag, will reject image before attempting
    flash write if slot address differs from the base address of the image.
    The feature can be used to mark Direct-XIP images with slot address
    they are intended for.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- cmd/img_mgmt/port/zephyr/src/zephyr_img_mgmt.c
+++ cmd/img_mgmt/port/zephyr/src/zephyr_img_mgmt.c
@@ -495,6 +495,23 @@ img_mgmt_impl_upload_inspect(const struct img_mgmt_upload_req *req,
             return MGMT_ERR_ENOMEM;
         }
 
+
+        if (hdr->ih_flags & IMAGE_F_ROM_FIXED_ADDR) {
+            rc = flash_area_open(action->area_id, &fa);
+            if (rc) {
+                *errstr = img_mgmt_err_str_flash_open_failed;
+                return MGMT_ERR_EUNKNOWN;
+            }
+
+            if (fa->fa_off != hdr->ih_load_addr) {
+                *errstr = img_mgmt_err_str_image_bad_flash_addr;
+                return MGMT_ERR_EUNKNOWN;
+            }
+
+            flash_area_close(fa);
+        }
+
+
         if (req->upgrade) {
             /* User specified upgrade-only.  Make sure new image version is
              * greater than that of the currently running image.
